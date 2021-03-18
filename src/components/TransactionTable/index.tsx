41277861359a61e5db89277ab3c802daaf8cc4c8
Gabriel <gabriel.devloper@gmail.com>
2021-03-18 13:42:06 -0300

    feat: configurando miragejs, api fake

--- src/components/TransactionTable/index.tsx
+++ src/components/TransactionTable/index.tsx
@@ -1,7 +1,17 @@
+import { useEffect } from "react";
 import { Container } from "./styles";
 
 
 export function TransactionTable(){
+    useEffect(() => {
+        
+        fetch("http://localhost:3000/api/transactions")
+        .then(response => response.json())
+        .then(data =>console.log(data));
+
+    },[]);
+
+
     return(
         <Container>
             <table>
