5891380e577fc44cab0f660ab00128cca4787820
Gabriel <gabriel.devloper@gmail.com>
2021-03-18 23:46:31 -0300

    feat: instalação da biblioteca react modal e configuração

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,33 @@
+import Modal from "react-modal";
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
+import { useState } from 'react';
+
+//acessibilidade
+Modal.setAppElement('#root');
+
 
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenOrCloseTransactionModal(){
+    setIsNewTransactionModalOpen(!isNewTransactionModalOpen);
+  }
+
+
   return (
     <>
-      <Header />
+      <Header onOpenOrCloseNewTransactionModal={handleOpenOrCloseTransactionModal}/>
       <Dashboard />
+
+      <Modal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleOpenOrCloseTransactionModal}
+
+      >
+        <h2>Cadastrar transação</h2>
+      </Modal>
       <GlobalStyle />
     </>
   );
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,20 @@
 import Logo from '../../assets/logo.svg';
 import { Container, Content } from './styles';
 
-export function Header(){
+interface HeaderProps {
+    onOpenOrCloseNewTransactionModal: () => void;
+}
+
+
+export function Header({ onOpenOrCloseNewTransactionModal }: HeaderProps){
     return(
         <Container>
             <Content>
                 <img src={Logo} alt="dt money"/>
-                <button type="button">
+                <button type="button" onClick={onOpenOrCloseNewTransactionModal}>
                     Nova transação
                 </button>
+                
             </Content>
         </Container>
     );
